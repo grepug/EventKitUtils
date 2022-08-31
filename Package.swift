cc4b9ef7eb8d3eba015e80b2c77ed91d873ad8dc
Kai Shao <grepug@icloud.com>
2022-08-31 21:21:09 +0800

    using diffableList 3.3.7

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/grepug/UIKitUtils", branch: "dev_3.1.0"),
+        .package(url: "https://github.com/grepug/UIKitUtils", branch: "dev_3.3.7"),
         .package(
            url: "https://github.com/apple/swift-collections.git",
            .upToNextMajor(from: "1.0.0") // or `.upToNextMinor
